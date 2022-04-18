b9fe1296c6d2651e97ec57160c3780a5d4f54ffc
ellen-woodward <98536796+ellen-woodward@users.noreply.github.com>
2022-04-18 09:07:39 +0100

    Lab 14

--- Program.cs
+++ Program.cs
@@ -4,34 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GetInteger
+namespace Q4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            getIntegerInput();
-        }
-
-        static int getIntegerInput()
-        {
-            int output;
-
-            bool isInteger;
-
-            Console.Write("Enter an int : ");
-            string input = Console.ReadLine();
-
-            isInteger = int.TryParse(input, out output);
-
-            while (!isInteger)
-            {
-                Console.WriteLine();
-                input = Console.ReadLine();
-                isInteger = int.TryParse(input, out output);
-            }
-
-            return output;
         }
     }
 }
