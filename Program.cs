8a3ced8c6f9fa0f30f7327af63f4a6a85d75657e
ellen-woodward <98536796+ellen-woodward@users.noreply.github.com>
2022-04-18 09:06:30 +0100

    Techniques

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getIntegerInput();
+        }
+
+        static int getIntegerInput()
+        {
+            int output;
+
+            bool isInteger;
+
+            Console.Write("Enter an int : ");
+            string input = Console.ReadLine();
+
+            isInteger = int.TryParse(input, out output);
+
+            while (!isInteger)
+            {
+                Console.WriteLine();
+                input = Console.ReadLine();
+                isInteger = int.TryParse(input, out output);
+            }
+
+            return output;
+        }
+    }
+}
