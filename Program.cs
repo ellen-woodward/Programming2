ec368b4ffa1b15518c4f85e1cc9985178aab67cd
ellen-woodward <98536796+ellen-woodward@users.noreply.github.com>
2022-04-18 09:09:02 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Q4
+namespace GetInteger
 {
     class Program
     {
         static void Main(string[] args)
         {
+            getIntegerInput();
+        }
+
+        static int getIntegerInput()
+        {
+            int output;
+
+            bool isInteger;
+
+            Console.Write("Enter an int : ");
+            string input = Console.ReadLine();
+
+            isInteger = int.TryParse(input, out output);
+
+            while (!isInteger)
+            {
+                Console.WriteLine();
+                input = Console.ReadLine();
+                isInteger = int.TryParse(input, out output);
+            }
+
+            return output;
         }
     }
 }
